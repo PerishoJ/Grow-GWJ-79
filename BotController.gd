09f980a246405a44b512fa085a67a03f2d8d6366
Josiah Perisho <josiah.perisho@gmail.com>
2025-03-17 22:51:28 -0500

    added some very basic input object to separate player from bot being controlled.

--- BotController.gd
+++ BotController.gd
@@ -1,5 +1,6 @@
 extends CharacterBody3D
 
+# Great Godot Types Tutorial https://docs.godotengine.org/en/latest/tutorials/scripting/gdscript/gdscript_basics.html#annotations
 
 const SPEED = 5.0
 const JUMP_VELOCITY = 4.5
@@ -7,20 +8,22 @@ const JUMP_VELOCITY = 4.5
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/3d/default_gravity")
 
-
-func _physics_process(delta):
+## This is meant to be called during _process_physics.
+func handleInput( input:BotInput , delta):
   # Add the gravity.
   if not is_on_floor():
     velocity.y -= gravity * delta
-
   # Handle jump.
-  if Input.is_action_just_pressed("jump") and is_on_floor():
+  if input.jump and is_on_floor():
     velocity.y = JUMP_VELOCITY
-
   # Get the input direction and handle the movement/deceleration.
   # As good practice, you should replace UI actions with custom gameplay actions.
-  var input_dir = Input.get_vector("left", "right", "up", "down")
+  var input_dir = input.movement
   var direction = (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
+  # TODO need to adjust for the input angle so the bot starts going the right direction.
+  #   E.g. lerp the y-angle towards the input.rotation so that the robot slowely turns to face the correct direction
+  #
+  #  ~ Maybe I just want point and click, League style. Hmm
   if direction:
     velocity.x = direction.x * SPEED
     velocity.z = direction.z * SPEED
---
+++ BotInput.gd
@@ -0,0 +1,10 @@
+## Provides the Robot with Input. Meant to be provided during _process_physics()
+class_name BotInput
+extends RefCounted
+
+## The forward/Backward values given, from th point of view of the camera (player)
+var movement: Vector3
+## Whether the jump key was just pressed
+var jump: bool
+## The global angle of the player's camera
+var referenceYAngle: float
