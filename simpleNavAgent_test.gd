0c39f4b86f3ce01a79e96e6d8f9db44d04b7851b
Josiah Perisho <josiah.perisho@gmail.com>
2025-04-04 20:08:41 -0500

    added simple navigation script

--- simpleNavAgent_test.gd
+++ simpleNavAgent_test.gd
@@ -1,4 +1,16 @@
 extends CharacterBody3D
 # check here https://www.youtube.com/watch?v=EOocBMBbL-E
 @onready var nav : NavigationAgent3D  = $NavAgent
+@export var target: Node3D
+@export var speed : float
+@export var accel : float
 
+func _physics_process(delta: float) -> void:
+  var direction = Vector3()
+  nav.target_position = target.global_position
+
+  direction = nav.get_next_path_position() - global_position
+  direction = direction.normalized()
+
+  velocity = velocity.lerp ( direction * speed , accel * delta)
+  move_and_slide()
